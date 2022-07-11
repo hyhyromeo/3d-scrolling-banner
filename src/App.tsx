396959869142a0aec27b82025b77fb2187ede0b3
Romeo Lau <hyromeo@hotmail.com>
2022-07-12 03:07:20 +0800

    project create

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
+import bananas from "./bananas.png";
+
+function Banner(props: { rotation: number }) {
+  const BannerStyle = {
+    width: "200px",
+    height: "100px",
+    transform: `rotateY(${props.rotation}deg)`,
+  };
+
+  return (
+    <div className="bananaWrap" style={BannerStyle}>
+      <div className="front">
+        <img width="100%" height="100%" alt={"banana"} src={bananas}></img>
+      </div>
+      <div className="back">
+        <img width="100%" height="100%" alt={"banana"} src={bananas}></img>
+      </div>
+      <div className="top"></div>
+      <div className="bottom"></div>
+      <div className="left">
+        <img width="100%" height="100%" alt={"banana"} src={bananas}></img>
+      </div>
+      <div className="right">
+        <img width="100%" height="100%" alt={"banana"} src={bananas}></img>
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  const [scrollY, setScrollY] = useState(0);
+  document.addEventListener("scroll", () => setScrollY(window.scrollY));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">Scroll Up or Down :D</div>
+      <div>
+      <div className="container">
+        <Banner rotation={scrollY} />
+      </div>
+
+      </div>
+     
     </div>
   );
 }
